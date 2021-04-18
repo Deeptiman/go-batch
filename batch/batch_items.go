f12f41e5058269eab1186c3a041219337ac61d1b
Deeptiman <pattnaikdeeptiman@gmail.com>
2021-04-19 02:20:26 +0530

    Logging formatter

--- batch/batch_items.go
+++ batch/batch_items.go
@@ -2,7 +2,7 @@ package batch
 
 // BatchItems struct defines the each batch item payload with an Id and relates to an overall BatchNo
 type BatchItems struct {
-	Id      int
-	BatchNo int
-	Item    interface{}
+	Id      int         `json:"id"`
+	BatchNo int         `json:"batchNo"`
+	Item    interface{} `json:"item"`
 }
--- batch/batch_utils.go
+++ batch/batch_utils.go
@@ -1,1 +1,8 @@
 package batch
+
+import "encoding/json"
+
+func getJsonString(item interface{}) string {
+	batchItem, _ := json.Marshal(item)
+	return string(batchItem)
+}
